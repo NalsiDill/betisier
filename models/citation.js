09c43270a28828ad7a77e0df68d0a03afadca4a8
Arnaud Strill <arnaud.strill@gmail.com>
2015-02-04 14:52:00 +0100

    Lister citation
    Page de listing prête, reste à récupérer le nom de l'auteur et la
    moyenne des notes

--- models/citation.js
+++ models/citation.js
@@ -1,12 +1,11 @@
 var db = require('../configDb');
 
 module.exports.getListeCitation = function (callback) {
-    
-    db.getConnection (function (err, connexion) {
-        if (!err)
-        {
-            connexion.query('SELECT * FROM citation', callback);
-            
+
+    db.getConnection(function (err, connexion) {
+        if (!err) {
+            connexion.query("SELECT per_num, cit_libelle, DATE_FORMAT(cit_date, '%d/%m/%Y') AS cit_date FROM citation WHERE cit_valide = 1", callback);
+
             connexion.release();
         }
     });
