5c2370bfdfdc6d4d3bf385a542ff8e192721767f
Arnaud Strill <arnaud.strill@gmail.com>
2015-02-04 15:03:58 +0100

    Lister les citations
    Rapatriement des noms d'auteurs OK, reste la moyenne des notes

--- models/citation.js
+++ models/citation.js
@@ -4,7 +4,7 @@ module.exports.getListeCitation = function (callback) {
 
     db.getConnection(function (err, connexion) {
         if (!err) {
-            connexion.query("SELECT per_num, cit_libelle, DATE_FORMAT(cit_date, '%d/%m/%Y') AS cit_date FROM citation WHERE cit_valide = 1", callback);
+            connexion.query("SELECT per_nom, per_prenom, cit_libelle, DATE_FORMAT(cit_date, '%d/%m/%Y') AS cit_date FROM citation c JOIN PERSONNE p ON c.per_num = p.per_num WHERE cit_valide = 1", callback);
 
             connexion.release();
         }
