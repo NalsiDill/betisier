9451e4b588e6ea626f6acad07f6d6e0fa44ace41
root <root@debGigot>
2015-03-10 14:24:58 +0100

    connexion ok

--- router/router.js
+++ router/router.js
@@ -23,6 +23,7 @@ module.exports = function(app){
 
 // connection
    app.get('/connect', ConnectController.Connect);
+   app.post('/tentativeConnexion', ConnectController.TentativeConnexion);
    app.get('/deconnect', ConnectController.Deconnect);
 
 
